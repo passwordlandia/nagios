93f2ef4b8d2870ad7a56fefbcbb2fafc86c012b6
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-06 16:24:17 -0700

    Update scp_to_nagios.sh

--- scp/scp_to_nagios.sh
+++ scp/scp_to_nagios.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-
+###prior to running this command I need to have my server already written in my gcloud init file,###
+###and add it in to there. I also need to define my IP's better.####
 
 /Users/pippy/generate_config.sh $1 $2
 
-gcloud compute copy-files $1.cfg nicolebade@nagios-b:/etc/nagios/conf.d
+gcloud compute copy-files $1.cfg paulierev1775@:nagios-scp-server/etc/nagios/conf.d
 
-# Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
-# I also had to chmod 770 /etc/nagios/conf.d
+usermod -a -G nagios paulierev1775
+chmod 770 /etc/nagios/conf.d
 
-gcloud compute ssh nicolebade@nagios-b "sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg"
+gcloud compute ssh paulierev1775@nagios-scp-server "sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg"
