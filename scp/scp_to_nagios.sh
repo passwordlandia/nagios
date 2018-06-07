bfc61f0fc2a4c5103f44f3cdb3c98efe896d1b7a
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-06 23:54:44 -0700

    added white space
    added a line to make it look neater

--- scp/scp_to_nagios.sh
+++ scp/scp_to_nagios.sh
@@ -3,6 +3,7 @@
 /Users/passwordlandia/nagios/scp/generate_config.sh $1 $2
 
 gcloud compute scp ~/$1.cfg \nagios-a:/etc/nagios/conf.d --ssh-key-file=/Users/paulierev1775/ .ssh/google_compute_engine --zone us-east1-b
+
 usermod -a -G nagios paulierev1775
 chmod 770 /etc/nagios/conf.d
 
